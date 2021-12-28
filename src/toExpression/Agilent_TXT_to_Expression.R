a700bba5055fd7dcccacf5ff77f71fd5d84868dc
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2021-12-28 14:59:34 +0100

    Updated help section and some comments in GATTACA and entry Bash-scripts, + other very minor fixes.

--- src/toExpression/Agilent_TXT_to_Expression.R
+++ src/toExpression/Agilent_TXT_to_Expression.R
@@ -1,3 +1,30 @@
+#!/usr/bin/env Rscript
+
+# License ----------------------------------------------------------------------
+# MIT License
+#
+# Copyright (c) 2021 Feat-FeAR
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# ------------------------------------------------------------------------------
+
 # Header Info ------------------------------------------------------------------
 #
 # Agilent TXT raw data --to--> expression matrix
@@ -19,9 +46,9 @@
 #   - Quantile-Quantile interarray normalization
 #   - Negative control probe evaluation
 #   - Invalid probes removal
-#   - Add annotation
 #   - Expression matrix saving
 #
+# ------------------------------------------------------------------------------
 
 log_debug("Sourcing the 'Agilent_TXT_to_Expression.R' file.")
 
