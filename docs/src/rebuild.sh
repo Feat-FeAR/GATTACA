57aa58bbbd05446ac8c00fd31ddfff53cf559701
Luca Visentin <visentin.hl@gmail.com>
2021-12-30 00:17:16 +0100

    Doc build script reformat

--- docs/src/rebuild.sh
+++ docs/src/rebuild.sh
@@ -1,16 +1,26 @@
 # This is a remider on how to rebuild the GATTACA_manual.pdf
 
 # First, run Xelatex to get a preliminary build
-xelatex main.tex
+echo -n "XeLaTeX - Pass 1/3..."
+xelatex main.tex > /dev/null
+echo "  Done."
 
 # 2 Then, run biber on the resulting files, to build biber build files:
-biber main
+echo -n "Generating bibliograpy with biber..."
+biber main > /dev/null
+echo "  Done."
 
 # 3 Rerun Xelatex
-xelatex main.tex
+echo -n "XeLaTeX - Pass 2/3..."
+xelatex main.tex > /dev/null
+echo "  Done."
 
 # 4 Rerun it again to finalize bibliographic refs
-xelatex main.tex
+echo -n "XeLaTeX - Pass 3/3..."
+xelatex main.tex > /dev/null
+echo "  Done."
 
 # 5 Finally, move the result over
+echo -n "Move rebuilt file..."
 mv ./main.pdf ../GATTACA_Manual.pdf
+echo "  Done."
